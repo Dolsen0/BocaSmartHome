8d53343614621bea50b73f11fc2c84c2bfb73dd2
dolsen0 <derek.olsen@gmail.com>
2022-11-05 00:42:47 -0400

    added stock photo into project

--- src/pages/Video.jsx
+++ src/pages/Video.jsx
@@ -1,4 +1,5 @@
 
+
 export default function Video(){
 
     const paragraphOne = "Yeah! Attorney-client privilege. I mean, that's a big one. That's something I provide for you. If I give up Pinkman, well, then you're gonna be asking, 'Ol' Saul gives 'em up pretty easy. What's to keep him from giving me up?' Y'see, so, then where's the trust? "
@@ -7,10 +8,19 @@ export default function Video(){
 
     return(
         <>
+        <div className="sections">
+
         <div id="videoReview">
             <h4>Reviews</h4>
         <p>{paragraphOne}</p>
         <p>{paragraphTwo}</p>
+        </div>
+        <div id="image">
+            <h4>
+                Image
+            </h4>
+            <img src='../images/hometheater.jpg' alt="Cinema Projector" />
+        </div>
         </div>
         </>
     )
